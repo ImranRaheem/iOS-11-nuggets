7e9bb97ec971ead6d4487da10a2ade2171f889b9
Imran Raheem <iraheem@seven.com.au>
2017-07-06 15:02:16 +1000

    initial commit

---
+++ Emojifier/Emojifier/CameraType.swift
@@ -0,0 +1,29 @@
+//
+//  CameraType.swift
+//  BestChannelFinder
+//
+//  Created by imran on 16/6/17.
+//  Copyright © 2017 Imran. All rights reserved.
+//
+
+import AVFoundation
+
+
+enum CameraType : Int {
+    case back
+    case front
+    
+    func captureDevice() -> AVCaptureDevice {
+        switch self {
+        case .front:
+            let devices = AVCaptureDevice.DiscoverySession(deviceTypes: [], mediaType: AVMediaType.video, position: .front).devices
+            print("devices:\(devices)")
+            for device in devices where device.position == .front {
+                return device
+            }
+        default:
+            break
+        }
+        return AVCaptureDevice.default(for: AVMediaType.video)!
+    }
+}
